81245233bf3b5b6c15f084e9e83d80b9e3be73ae
DevMau <maurizio.vdwerf@asd.nl>
2025-02-02 14:10:48 +0100

    setup grid

--- 01-ChristmasLightKata/ChristmasLightGrid.py
+++ 01-ChristmasLightKata/ChristmasLightGrid.py
@@ -1,7 +1,14 @@
 class ChristmasLightGrid:
 
-    def deployGrid(self, y):
+    def deployGrid(self, y=0, x=0):
         self.y = y
+        self.x = x
 
-    def GetSizeY(self):
-        return self.y
+    def getSize(self):
+        return f"y={self.y} x={self.x}"
+
+    def turnOnLights(self):
+        return "Lights turned on"
+
+    def returnLightsOnCount(self):
+        return self.y*self.x
--- 01-ChristmasLightKata/TestsChristmasLightKata.py
+++ 01-ChristmasLightKata/TestsChristmasLightKata.py
@@ -7,6 +7,15 @@ def lightGrid():
     lightGrid = ChristmasLightGrid()
     return lightGrid
 
-def test_deployGridSizeY(lightGrid):
-    lightGrid.deployGrid(1000)
-    assert lightGrid.GetSizeY() == 1000
+def test_deployGrid(lightGrid):
+    lightGrid.deployGrid(1000, 1000)
+    assert lightGrid.getSize() == "y=1000 x=1000"
+
+def test_turnOnAllLightsReturnsMessage(lightGrid):
+    lightGrid.turnOnLights()
+    assert lightGrid.turnOnLights() == "Lights turned on"
+
+def test_returnHowManyLightsTurnedOn(lightGrid):
+    lightGrid.deployGrid(1000, 1000)
+    lightGrid.returnLightsOnCount()
+    assert lightGrid.returnLightsOnCount() == 1000000
